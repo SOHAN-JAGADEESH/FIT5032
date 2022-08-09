bb9595e59d4264123081ddac475445bad52e792d
sohan <sohan@SOHAN>
2022-08-09 20:27:27 +1000

    Add project files.

---
+++ codesnippet/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codesnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            Models.Exercise.Student student = new Models.Exercise.Student("Sohan", "0478842939");
+
+            Models.Exercise.ExampleDictionary dict = new Models.Exercise.ExampleDictionary();
+            ViewBag.Message = dict.Example();
+
+            return View();
+
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ codesnippet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="codesnippet.MvcApplication" Language="C#" %>
